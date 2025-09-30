5dadab57d6e25caf8692ce7038a6c99d7751233d
kee <chinboy2012@gmail.com>
2025-09-30 12:14:04 +0800

    config setting && test

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,12 +1,18 @@
 <?php
+/*
+|--------------------------------------------------------------------------
+| Register Middleware
+|--------------------------------------------------------------------------
+|
+| Next, we will register the middleware with the application. These can
+| be global middleware that run before and after each request into a
+| route or middleware that'll be assigned to some specific routes.
+|
+ */
 
-require_once __DIR__.'/../vendor/autoload.php';
-
-(new Laravel\Lumen\Bootstrap\LoadEnvironmentVariables(
-    dirname(__DIR__)
-))->bootstrap();
-
-date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
+defined('BASE_PATH') || define('BASE_PATH', realpath(dirname(__DIR__)));
+defined('VENDOR_PATH') || define('VENDOR_PATH', BASE_PATH . '/vendor/');
+require_once VENDOR_PATH . '/autoload.php';
 
 /*
 |--------------------------------------------------------------------------
@@ -17,99 +23,34 @@ date_default_timezone_set(env('APP_TIMEZONE', 'UTC'));
 | that serves as the central piece of this framework. We'll use this
 | application as an "IoC" container and router for this framework.
 |
-*/
+ */
+/**
+ * public Bootstrap (AppPath, envPath) initialize
+ * */
+$app = new Laravel\Lumen\Bootstrap(BASE_PATH, dirname(__DIR__));
 
-$app = new Laravel\Lumen\Application(
-    dirname(__DIR__)
-);
-
-// $app->withFacades();
-
-// $app->withEloquent();
-
-/*
-|--------------------------------------------------------------------------
-| Register Container Bindings
-|--------------------------------------------------------------------------
-|
-| Now we will register a few bindings in the service container. We will
-| register the exception handler and the console kernel. You may add
-| your own bindings here if you like or you can make another file.
-|
-*/
-
-$app->singleton(
-    Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
-);
-
-$app->singleton(
-    Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
-);
-
-/*
-|--------------------------------------------------------------------------
-| Register Config Files
-|--------------------------------------------------------------------------
-|
-| Now we will register the "app" configuration file. If the file exists in
-| your configuration directory it will be loaded; otherwise, we'll load
-| the default version. You may register other files below as needed.
-|
-*/
-
-$app->configure('app');
-
-/*
-|--------------------------------------------------------------------------
-| Register Middleware
-|--------------------------------------------------------------------------
-|
-| Next, we will register the middleware with the application. These can
-| be global middleware that run before and after each request into a
-| route or middleware that'll be assigned to some specific routes.
-|
-*/
-
-// $app->middleware([
-//     App\Http\Middleware\ExampleMiddleware::class
-// ]);
-
-// $app->routeMiddleware([
-//     'auth' => App\Http\Middleware\Authenticate::class,
-// ]);
-
-/*
-|--------------------------------------------------------------------------
-| Register Service Providers
-|--------------------------------------------------------------------------
-|
-| Here we will register all of the application's service providers which
-| are used to bind services into the container. Service providers are
-| totally optional, so you are not required to uncomment this line.
-|
-*/
-
-// $app->register(App\Providers\AppServiceProvider::class);
-// $app->register(App\Providers\AuthServiceProvider::class);
-// $app->register(App\Providers\EventServiceProvider::class);
-
-/*
-|--------------------------------------------------------------------------
-| Load The Application Routes
-|--------------------------------------------------------------------------
-|
-| Next we will include the routes file so that they can all be added to
-| the application. This will provide all of the URLs the application
-| can respond to, as well as the controllers that may handle them.
-|
-*/
-
-$app->router->group([
-    'namespace' => 'App\Http\Controllers',
-], function ($router) {
-    require __DIR__.'/../routes/web.php';
-});
-
-return $app;
+return $app
+  ->with('eloquent', 'auth')
+  ->singletons([
+    [ // 初始化异常处理
+      Illuminate\Contracts\Debug\ExceptionHandler::class,
+      App\Exceptions\Handler::class
+    ],
+    [ // 初始化命令行核心
+      Illuminate\Contracts\Console\Kernel::class,
+      App\Console\Kernel::class
+    ]
+  ])
+  // 初始化日志模块
+  // ->configureMonologUsing(function ($monolog) {                                  
+  //   $handler = new \Package\LumenExtra\Handler\MongoHandler('mongodb', 'api-logs');
+  //   $monolog->pushHandler($handler);
+  //   return $monolog;
+  // })
+  ->tap(function ($app) {
+    return $app->router->group([
+      'namespace' => 'App\Http\Controllers',
+      'routes' => 'routes/web.php',
+    ]);
+  });
+  //->run();
--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,21 @@
 | It is a breeze. Simply tell Lumen the URIs it should respond to
 | and give it the Closure to call when that URI is requested.
 |
-*/
+ */
 
 $router->get('/', function () use ($router) {
-    return $router->app->version();
+  //return $router->app->version();
+  return [
+    'php' => phpversion(),
+    'version' => $router->app->version(),
+    'server' => request()->server(),
+    'user' => Auth::user(),
+    'cache' => Cache::remember('foo', 10, fn() => mt_rand(100,999)),
+    'config' => [
+      'app' => config('app'),
+      'database' => config('database'),
+      'validation' => config('validation'),
+      'exceptions' => config('exceptions'),
+    ]
+  ];
 });
